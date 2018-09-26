4d60fe8c77d4b1fee1ca15c53c8137db9598e8bc
suave-cito <rob_car_19@hotmail.com>
2018-09-25 21:32:44 -0500

    borrado

--- wp-content/themes/accesspress-store/woocommerce/woocommerce-hooks.php
+++
@@ -1,5 +0,0 @@
-<?php
-	/**   =======   Woocommerce Hooks    =======    **/
-
-	remove_action('woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
-	remove_action('woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
