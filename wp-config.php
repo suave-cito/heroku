4280087adf488bd635b991aa76ae6bc72789ff9c
suave-cito <rob_car_19@hotmail.com>
2018-09-25 20:33:07 -0500

    Nueva Config

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wordpress');
+define('DB_NAME', 'heroku_caf3d34a07718d1');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'b129f5a7efd879');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '8f310b76');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'us-cdbr-iron-east-01.cleardb.net');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
